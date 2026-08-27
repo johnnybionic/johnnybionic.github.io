9cba7bc70fb11fb7cbcc4a0c3ea9689b86ce2212
john d <jdavddlo@gmail.com>
2026-08-27 14:13:57 +0100

    moved partial snippets to 'partials'

--- assets/js/load-footer.js
+++ assets/js/load-footer.js
@@ -1,5 +1,5 @@
-// Load header from external HTML file
-fetch('/footer.html')
+// Load footer from external HTML file
+fetch('/partials/footer.html')
 	.then(response => response.text())
 	.then(data => {
 		document.getElementById('footer-container').innerHTML = data;
